d01674868b3d135aa070cca1def35ac9563cc745
qitar888 <qitar888@gmail.com>
2014-05-18 22:34:37 +0800

    activities part update

--- script/func.js
+++ script/func.js
@@ -90,3 +90,30 @@ function init() {
 	});
 	marker.setMap(map);
 }
+function pop(elem){
+	$(elem).bPopup({modalColor: '#0FF'});
+
+}
+    // Semicolon (;) to ensure closing of earlier scripting
+    // Encapsulation
+    // $ is assigned to jQuery
+/*    ;(function($) {
+
+         // DOM Ready
+        $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#my-button').on('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#element_to_pop_up').bPopup({modalColor: '#0FF'});
+
+            });
+
+        });
+
+    })(jQuery);*/
